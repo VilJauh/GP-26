89397ffbaa02e4b667b9acbf068577f8e252055f
VilJauh <157316437+VilJauh@users.noreply.github.com>
2026-01-19 19:10:28 +0200

    settings changed

--- Assets/FPS/Scripts/Gameplay/AmmoPickup.cs
+++ Assets/FPS/Scripts/Gameplay/AmmoPickup.cs
@@ -10,7 +10,7 @@ namespace Unity.FPS.Gameplay
 
         [Tooltip("Number of bullets the player gets")]
         public int BulletCount = 30;
-
+        
         protected override void OnPicked(PlayerCharacterController byPlayer)
         {
             PlayerWeaponsManager playerWeaponsManager = byPlayer.GetComponent<PlayerWeaponsManager>();
