18c47d7d6e72353f1b69227903946012450fcbb9
VilJauh <157316437+VilJauh@users.noreply.github.com>
2026-02-02 12:18:42 +0200

    Added cup script

---
+++ Assets/Scripts/Cup.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace AE9730
+{
+    public class Cup : MonoBehaviour
+    {
+        public Bottle bottleToInteractWith;
+        [Range(-3.0f, 3.0f)]
+        public float fillValue;
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    } 
+}
